d87db9226b849ea671b8012fb19a784003ca4498
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-02-02 07:58:13 +0800

    add 17temp.cpp

--- template/17temp.cpp
+++ template/17temp.cpp
@@ -69,33 +69,21 @@ struct printer {
     printer(T&& iterable): iterable(forward<T>(iterable)) {}
 };
 
-inline void read() {}
-template<class T, class ...U> inline void read(T& head, U&... tail) {
-    cin >> head; read(tail...);
+template<class ...T> void read(T& ...args) {
+    (cin >> ... >> args);
 }
-template<class T> inline void print_1(const T& x) { cout << x; }
-template<class T> inline void print_1(const vector<T>& v) {
-    for(auto it = v.begin(); it != v.end(); ++it) {
-        if(it != v.begin()) putchar(' '); print_1(*it);
-    }
+template<class T, class ...U> void print_n(const T& head, const U& ...args) {
+    cout << head;
+    ((cout << ' ' << args), ...);
 }
-inline void print_n() {}
-template<class T, class ...U> inline void print_n(const T& head, const U&... tail) {
-    print_1(head); if(sizeof...(tail)) putchar(' '); print_n(tail...);
+template<class ...T> void print(const T& ...args) {
+    print_n(args...);
+    cout << endl;
 }
-template<class ...T> inline void print(const T&... args) { print_n(args...); putchar('\n'); }
+
 static int fastio = [](){ ios_base::sync_with_stdio(false); cin.tie(0); return 0; }();
 
 #define repeat(x) [[maybe_unused]] auto _: range(x)
-
-// for c++14 (no template constructor deduction)
-#define GET_MACRO(_1,_2,_3,NAME,...) NAME
-#define range3(x,y,z) range<decltype(x)>(x,y,z)
-#define range2(x,y) range<decltype(x)>(x,y)
-#define range1(x) range<decltype(x)>(x)
-#define range(...) GET_MACRO(__VA_ARGS__, range3, range2, range1)(__VA_ARGS__)
-#define enumerate(x) enumerate<decltype(x)>(x)
-#define printer(x) printer<decltype(x)>(x)
 //}}}
 using PII = pair<int, int>;
 using LL = long long;
