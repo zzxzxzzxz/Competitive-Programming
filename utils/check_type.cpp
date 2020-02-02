5bda5aa0bce531db6bd5c27ec5db4cf81d540f74
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-02-02 08:07:52 +0800

    update check_type.cpp

--- utils/check_type.cpp
+++ utils/check_type.cpp
@@ -1,9 +1,19 @@
 #include <vector>
+using namespace std;
+
+template<class T>
+auto test(T&& t) {
+    return *t.begin();
+}
 
 template<typename T> struct TD;
 
 int main() {
-    std::vector<int> v;
-    TD<decltype(v)> td;
+    vector<vector<int>> v = {{1, 2}, {3}};
+    auto&& x = test(v);
+    auto&& y = v[0];
+
+    TD<decltype(x)> td1;
+    TD<decltype(y)> td2;
     return 0;
 }
