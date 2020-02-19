eb7fea8b869dabe25fa2aafdd1d2f135c31785eb
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-02-20 05:15:07 +0800

    print with "if constexpr"

--- tmp/print.cpp
+++ tmp/print.cpp
@@ -1,38 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T, typename = void> struct is_std_container : false_type {};
-template <typename T> struct is_std_container<T, void_t<decltype(begin(declval<T>()))>> : true_type {};
-
-template<class T> using IsC = typename enable_if<is_std_container<T>::value and
-    not std::is_same<T, string>::value>::type;
-template<class T> using NotC = typename enable_if<not is_std_container<T>::value or
-    std::is_same<T, string>::value>::type;
+template<typename T, typename = void> struct is_container : false_type {};
+template<typename T> struct is_container<T, void_t<decltype(begin(declval<T>()))>> : true_type {};
+template<typename T> struct is_tuple : false_type {};
+template<typename ...T> struct is_tuple<tuple<T...>> : true_type {};
+template<typename T> struct is_pair : false_type {};
+template<typename T, typename U> struct is_pair<pair<T, U>> : true_type {};
 
 inline void print_n() {}
-template<class T, class ...U> void print_n(const T& head, const U& ...args);
+template<class T, class ...U> inline void print_n(const T& head, const U& ...args);
 
-template<class T> inline NotC<T> print_1(const T& x) { cout << x; }
+template<class T> inline void print_1(const T& x) {
+    if constexpr(is_same<T, string>::value) {
+        cout << x;
 
-template<class T> inline IsC<T> print_1(const T& v) {
-    for(auto it = v.begin(); it != v.end(); ++it) {
-        if(it != v.begin()) cout << ' '; print_1(*it);
+    } else if constexpr(is_container<T>::value) {
+        for(auto it = x.begin(); it != x.end(); ++it) {
+            if(it != x.begin()) cout << ' '; print_1(*it);
+        }
+
+    } else if constexpr(is_tuple<T>::value) {
+        auto print_tuple = [](const auto& ...ts) { print_n(ts...); };
+        apply(print_tuple, x);
+
+    } else if constexpr(is_pair<T>::value) {
+        print_n(x.first, x.second);
+
+    } else {
+        cout << x;
     }
 }
 
-template<class ...T> inline void print_1(const tuple<T...>& x) {
-    auto print_tuple = [](const auto& ...ts) { print_n(ts...); };
-    apply(print_tuple, x);
-}
-
-template<class T, class U> inline void print_1(const pair<T, U>& p) {
-    print_n(p.first, p.second);
-}
-
-template<class T, class ...U> void print_n(const T& head, const U& ...args) {
+template<class T, class ...U> inline void print_n(const T& head, const U& ...args) {
     print_1(head); ((cout << ' ', print_1(args)), ...);
 }
-
 template<class ...T> inline void print(const T& ...args) {
     print_n(args...); cout << '\n';
 }
