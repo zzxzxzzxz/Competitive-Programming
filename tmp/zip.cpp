d000ef68bc8b7214025f49d772e1089e7a27609a
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-02-23 08:44:41 +0800

    update zip.cpp

--- tmp/zip.cpp
+++ tmp/zip.cpp
@@ -3,19 +3,84 @@
 #include <set>
 using namespace std;
 
+template<typename T>
+constexpr auto range(T start, T stop, T step) {
+    struct iterator {
+        T i, step;
+        T operator-(const iterator& other) { return i - other.i; };
+        bool operator!=(const iterator& other) const { return i != other.i; }
+        auto& operator+=(const int& n) { i += step * n; return *this; }
+        auto& operator++() { i += step; return *this; }
+        auto& operator*() { return i; }
+
+        using difference_type = T;
+        using value_type = T;
+        using pointer = T*;
+        using reference = T&;
+        using iterator_category = random_access_iterator_tag;
+    };
+    struct iterable_wrapper {
+        T start, stop, step;
+        auto begin() const { return iterator{start, step}; }
+        auto end() const { return iterator{stop, step}; }
+        size_t size() const { return (stop - start) / step; }
+        iterable_wrapper(T start_, T stop_, T step_): start(start_), stop(stop_), step(step_) {
+            stop = step > 0 ? max(start, stop) : min(start, stop);
+            stop += (step - (stop - start) % step) % step;
+        }
+    };
+    return iterable_wrapper(start, stop, step);
+};
+template<typename T> constexpr auto range(T start, T stop) { return range(start, stop, T(1)); }
+template<typename T> constexpr auto range(T stop) { return range(T(0), stop, T(1)); }
+
+template<typename T, typename = decltype(rbegin(declval<T>()))>
+constexpr auto reversed(T&& iterable) {
+    struct iterable_wrapper {
+        T iterable;
+        auto begin() const { return std::rbegin(iterable); }
+        auto end() const { return std::rend(iterable); }
+        auto size() const { return std::size(iterable); };
+    };
+    return iterable_wrapper{ forward<T>(iterable) };
+}
+
+template<typename T, typename Iter = decltype(begin(declval<T>()))>
+constexpr auto printer(T&& iterable) {
+    struct iterator {
+        Iter iter, ed;
+        auto operator!=(const iterator& other) {
+            auto ret = (iter != other.iter);
+            if(not ret) cout << '\n';
+            return ret;
+        }
+        auto& operator++() { ++iter; if(iter != ed) cout << ' '; return *this; }
+        auto& operator*() { return *iter; }
+    };
+    struct iterable_wrapper {
+        T iterable;
+        auto begin() const {
+            return iterator{ std::begin(iterable), std::end(iterable) };
+        }
+        auto end() const {
+            return iterator{ std::end(iterable), std::end(iterable) };
+        }
+    };
+    return iterable_wrapper{ forward<T>(iterable) };
+};
+
 template<size_t ...Is, class T, class ...Cs,
     class Iter = tuple<decltype(begin(declval<Cs>()))...>,
-    class Val = tuple<decltype(*begin(declval<Cs>()))&...>
+    class Ref = tuple<decltype(*begin(declval<Cs>()))&...>
     >
 constexpr auto zip(index_sequence<Is...>, T&& iterables, Cs&&...) {
     struct iterator {
         Iter iter;
-        unique_ptr<Val> tref = nullptr;
+        unique_ptr<Ref> tref = nullptr;
         bool operator!=(const iterator& other) { return ((get<Is>(iter) != get<Is>(other.iter)) and ...); }
         auto& operator++() { ((++get<Is>(iter)), ...); return *this; }
         auto& operator*() {
-            tref.reset();
-            tref = make_unique<Val>(tie(*get<Is>(iter)...));
+            tref.reset(new Ref(tie(*get<Is>(iter)...)));
             return *tref;
         }
     };
@@ -23,21 +88,30 @@ constexpr auto zip(index_sequence<Is...>, T&& iterables, Cs&&...) {
         T iterables;
         auto begin() const { return iterator { Iter{ std::begin(get<Is>(iterables))... } }; }
         auto end() const { return iterator { Iter{ std::end(get<Is>(iterables))... } }; }
+        auto size() const { return min({ std::size(get<Is>(iterables))... }); }
     };
     return iterable_wrapper{ forward<T>(iterables) };
 }
 template<class ...Cs, class = tuple<decltype(begin(declval<Cs>()))...>>
 constexpr auto zip(Cs&& ...cs) {
-    return zip(index_sequence_for<Cs...>{}, tie(cs...), forward<Cs>(cs)...);
+    return zip(index_sequence_for<Cs...>{}, tuple<Cs...>(forward<Cs>(cs)...), forward<Cs>(cs)...);
+}
+
+template<typename T, typename Iter = decltype(begin(declval<T>()))>
+constexpr auto enumerate(T&& iterable) {
+    return zip(range(std::size(iterable)), forward<T>(iterable));
 }
 
 int main() {
-    vector<int> v = {1, 2, 3};
-    for(auto [x] : zip(v)) cout << x << " "; cout << endl;
-    for(auto [x] : zip(zip(v))) cout << get<0>(x) << " "; cout << endl;
-    for(auto [x] : zip(zip(v))) cout << get<0>(x) << " "; cout << endl;
-    v[1] = 100;
-    for(auto [x] : zip(v)) cout << x << " "; cout << endl;
-    for(auto [x] : zip(zip(v))) cout << get<0>(x) << " "; cout << endl;
+    vector<int> v1 = {4, 5, 6, 7};
+    vector<int> v2 = {8, 9, 10};
+
+    auto p = printer(enumerate(zip(v1, reversed(v2))));
+    v2[1] = 100;
+
+    for(auto [i, x] : p) {
+        auto [a, b] = x;
+        cout << "(" << i << ", " << a << ", " << b << ")";
+    }
     return 0;
 }
