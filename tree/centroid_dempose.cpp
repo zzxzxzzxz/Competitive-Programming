ef789183af7b185138fd3c515f981151952c7c6c
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-02-26 23:10:05 +0800

    centroid decomposition

--- tree/centroid_dempose.cpp
+++ tree/centroid_dempose.cpp
@@ -1,67 +1,111 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1000000007;
-#define MAX_N 100005
+//{{{
+long long count(int v, int k);
+//}}}
+
+const int MAX_N = 50005;
+using LL = long long;
 
 struct edge {
-    int to, length;
+    int to;
 };
 
-int N, K;
 vector<edge> G[MAX_N];
-
-bool centroid[MAX_N];
+bool used[MAX_N];
 int subtree_size[MAX_N];
-int ans;
 
 int compute_subtree_size(int v, int p) {
-    int c = 1;
+    int sz = 1;
     for(auto& e: G[v]) {
-        if(e.to == p or centroid[e.to]) {
+        if(e.to == p or used[e.to]) {
             continue;
         }
-        c += compute_subtree_size(e.to, v);
+        sz += compute_subtree_size(e.to, v);
     }
-    return c;
+    subtree_size[v] = sz;
+    return sz;
 }
 
 pair<int, int> search_centroid(int v, int p, int total) {
     pair<int, int> res = {INT_MAX, -1};
-    int sz = 1, maxsz = 0;
+    int maxsz = total - subtree_size[v];
     for(auto& e: G[v]) {
-        if(e.to == p or centroid[e.to]) {
-            continue;
+        if(e.to != p and not used[e.to]) {
+            maxsz = max(maxsz, subtree_size[e.to]);
+            res = min(res, search_centroid(e.to, v, total));
         }
-        res = min(res, search_centroid(e.to, v, total));
-        maxsz = max(maxsz, subtree_size[e.to]);
-        sz += subtree_size[e.to];
     }
-    maxsz = max(maxsz, total - sz);
     res = min(res, make_pair(maxsz, v));
     return res;
 }
 
-int foo(int) { return 0; }
-
-void solve(int v) {
+int solve(int v, int k) {
     compute_subtree_size(v, -1);
-    int c = search_centroid(v, -1, subtree_size[v]).second;
-    centroid[c] = true;
+    int ctr = search_centroid(v, -1, subtree_size[v]).second;
+    used[ctr] = true;
 
-    for(auto& e: G[c]) {
-        if(centroid[e.to]) {
-            continue;
+    LL ans = count(ctr, k) / 2;
+
+    for(auto& e: G[ctr]) {
+        if(not used[e.to]) {
+            ans += solve(e.to, k);
         }
-        solve(e.to);
     }
-    ans += foo(c);
+    return ans;
 }
+
+//{{{
+LL count(int v, int k) {
+    unordered_map<int, LL> cnt;
+    auto dfs = [&](auto self, int v, int p, int depth) -> void {
+        if(depth <= k) ++cnt[depth];
+        for(auto& e : G[v]) {
+            if(e.to != p and not used[e.to]) {
+                self(self, e.to, v, depth + 1);
+            }
+        }
+    };
+
+    LL ans = 0;
+    unordered_map<int, LL> cnt_all = {{0, 1}};
+    for(auto& e : G[v]) {
+        if(used[e.to]) continue;
+
+        cnt.clear();
+        dfs(dfs, e.to, v, 1);
+        for(auto [key, val] : cnt) {
+            cnt_all[key] += val;
+            if(cnt.find(k - key) != cnt.end()) {
+                ans -= val * cnt[k - key];
+            }
+        }
+    }
+
+    for(auto [key, val] : cnt_all) {
+        if(cnt_all.find(k - key) != cnt_all.end()) {
+            ans += val * cnt_all[k - key];
+        }
+    }
+    return ans;
+}
+//}}}
+
+// https://codeforces.com/contest/161/problem/D
 
 int main()
 {
-    ans = 0;
-    solve(0);
+    int n = 5, k = 2;
+    vector<array<int, 2>> graph = {
+        {1, 2}, {2, 3}, {3, 4}, {2, 5},
+    };
+    for(int i = 0; i < n - 1; ++i) {
+        auto [u, v] = graph[i];
+        G[u].push_back({v});
+        G[v].push_back({u});
+    }
+    auto ans = solve(1, k);
     cout << ans << endl;
     return 0;
 }
