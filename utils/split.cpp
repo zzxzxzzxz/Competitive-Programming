8b2a123610a80fa58a8ca4116c3afe3d25a3bc84
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-03-13 20:30:00 +0800

    utils/split.cpp use string_view

--- utils/split.cpp
+++ utils/split.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-vector<string> split(const string& s, const string& delim) {
-    vector<string> tokens;
-    size_t start = 0;
-    //size_t end = s.find(delim, start);
-    size_t end = s.find_first_of(delim);
-    while(end != string::npos) {
-        tokens.push_back(s.substr(start, end-start));
-        //start = end + delim.size();
-        //end = s.find(delim, start);
-        start = end + 1;
-        end = s.find_first_of(delim, start);
+auto str_split(string_view strv, string_view delims = " \t") {
+    vector<string_view> res;
+    size_t prv = 0, idx;
+    while((idx = strv.find_first_of(delims, prv)) != string_view::npos) {
+        res.emplace_back(strv.substr(prv, idx - prv));
+        prv = idx + 1;
     }
-    tokens.push_back(s.substr(start, s.size()-start));
-    return tokens;
+    res.emplace_back(strv.substr(prv));
+    return res;
 }
 
-
-int main()
-{
-    char s[100] = ",;asdq,;dsgre,tjjjj,;";
-    vector<string> tokens = split(s, ",;");
-    for(auto ii = tokens.begin(); ii != tokens.end(); ++ii) {
-        printf("\"%s\"\n", ii->c_str());
+int main() {
+    char s[100] = "asdq dsgre,tjj\tjj ";
+    auto tokens = str_split(s);
+    for(const auto& s: tokens) {
+        cout << "\"" << s << "\"" << endl;
     }
     return 0;
 }
