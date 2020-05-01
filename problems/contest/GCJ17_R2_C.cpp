5e946fb9ba8c1fc03f8cc86da52484cd3aac7055
Yu-Chen Lu <zzxzxzzxz@gmail.com>
2020-05-01 10:16:44 +0800

    update GCJ17

--- problems/contest/GCJ17_R2_C.cpp
+++ problems/contest/GCJ17_R2_C.cpp
@@ -1,6 +1,6 @@
 //{{{
 #pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
 #include <bits/stdc++.h>
@@ -9,30 +9,10 @@ using namespace std;
 #define putchar(x) cout << (x)
 #define repeat(x) int _ = 0; _ < (x); ++_
 
-#define SELECT(_1, _2, _3, _4, _5, _6, _7, _8, NAME,...) NAME
-#define showvar(x) print_n(" ", #x, "=", x);
-#define dbg1(a) showvar(a) cout << endl;
-#define dbg2(a, b) showvar(a) cout << ", "; dbg1(b);
-#define dbg3(a, b, c) showvar(a) cout << ", "; dbg2(b, c);
-#define dbg4(a, b, c, d) showvar(a) cout << ", "; dbg3(b, c, d);
-#define dbg5(a, b, c, d, e) showvar(a) cout << ", "; dbg4(b, c, d, e);
-#define dbg6(a, b, c, d, e, f) showvar(a) cout << ", "; dbg5(b, c, d, e, f);
-#define dbg7(a, b, c, d, e, f, g) showvar(a) cout << ", "; dbg6(b, c, d, e, f, g);
-#define dbg8(a, b, c, d, e, f, g, h) showvar(a) cout << ", "; dbg7(b, c, d, e, f, g, h);
-#define debug(...) SELECT(__VA_ARGS__, dbg8, dbg7, dbg6, dbg5, dbg4, dbg3, dbg2, dbg1)(__VA_ARGS__)
-
 template<typename T> constexpr auto range(T start, T stop, T step) {
     struct iterator {
-        using difference_type = T;
-        using value_type = T;
-        using pointer = T*;
-        using reference = T&;
-        using iterator_category = random_access_iterator_tag;
-
         T i, step;
-        T operator-(const iterator& other) const { return i - other.i; };
         bool operator!=(const iterator& other) const { return i != other.i; }
-        auto& operator+=(const int& n) { i += step * n; return *this; }
         auto& operator++() { i += step; return *this; }
         auto& operator*() { return i; }
     };
@@ -51,17 +31,6 @@ template<typename T> constexpr auto range(T start, T stop, T step) {
 };
 template<typename T> constexpr auto range(T start, T stop) { return range(start, stop, T(1)); }
 template<typename T> constexpr auto range(T stop) { return range(T(0), stop, T(1)); }
-
-template<typename T, typename Iter = decltype(rbegin(declval<T>()))>
-auto reversed(T&& iterable) {
-    struct iterable_wrapper {
-        T iterable;
-        auto begin() const { return std::rbegin(iterable); }
-        auto end() const { return std::rend(iterable); }
-        auto size() const { return iterable.size(); }
-    };
-    return iterable_wrapper{ forward<T>(iterable) };
-}
 
 template<typename T, typename Iter = decltype(begin(declval<T>()))>
 constexpr auto printer(T&& iterable) {
@@ -83,56 +52,19 @@ constexpr auto printer(T&& iterable) {
     return iterable_wrapper{ forward<T>(iterable) };
 };
 
-template<class ...T> void absorb(T&& ...) {}
-template<size_t L, size_t I, class T>
-bool zip_it_ne(const T& it1, const T& it2) {
-    if(not (get<I>(it1) != get<I>(it2))) return false;
-    if(I + 1 == L) return true;
-    return zip_it_ne<L, (I + 1) % L, T>(it1, it2);
-}
-template<size_t ...Is, class T, class ...Cs,
-    class Iter = tuple<decltype(begin(declval<Cs>()))...>,
-    class Ref = tuple<decltype(*begin(declval<Cs>()))&...>
-    >
-constexpr auto zip(index_sequence<Is...>, T&& iterables, Cs&&...) {
-    struct iterator {
-        Iter iter;
-        unique_ptr<Ref> tref = nullptr;
-        bool operator!=(const iterator& other) const {
-            return zip_it_ne<sizeof...(Is), 0, Iter>(iter, other.iter);
-        }
-        auto& operator++() { absorb(++get<Is>(iter)...); return *this; }
-        auto& operator*() { tref.reset(new Ref(tie(*get<Is>(iter)...))); return *tref; }
-    };
-    struct iterable_wrapper {
-        T iterables;
-        auto begin() const { return iterator { Iter{ std::begin(get<Is>(iterables))... } }; }
-        auto end() const { return iterator { Iter{ std::end(get<Is>(iterables))... } }; }
-        auto size() const { return min({ get<Is>(iterables).size()... }); }
-    };
-    return iterable_wrapper{ forward<T>(iterables) };
-}
-template<class ...Cs, class = tuple<decltype(begin(declval<Cs>()))...>>
-constexpr auto zip(Cs&& ...cs) {
-    return zip(index_sequence_for<Cs...>{}, tuple<Cs...>(forward<Cs>(cs)...), forward<Cs>(cs)...);
-}
-
-template<typename T, typename Iter = decltype(begin(declval<T>()))>
-constexpr auto enumerate(T&& iterable) {
-    return zip(range(iterable.size()), forward<T>(iterable));
-}
-
 template<size_t ...Is, typename T> auto getis(const T& t) { return tie(get<Is>(t)...); }
 template<class T> void setmax(T& a, const T& b) { a = max(a, b); }
 template<class T> void setmin(T& a, const T& b) { a = min(a, b); }
 
+template<typename T> struct is_const_char_arr_ref : false_type {};
+template<size_t N> struct is_const_char_arr_ref<char const (&)[N]> : true_type {};
 template<typename T, typename = void> struct is_container : false_type {};
 template<typename T>
 struct is_container<T, conditional_t<false, decltype(begin(declval<T>())), void>> : true_type {};
 template<class T> using IsC = typename enable_if<is_container<T>::value and
-    (not is_same<T, string>::value) and (not is_same<T, string_view>::value)>::type;
+    not is_same<T, string>::value>::type;
 template<class T> using NotC = typename enable_if<not is_container<T>::value or
-    is_same<T, string>::value or is_same<T, string_view>::value>::type;
+    is_same<T, string>::value>::type;
 template<class T> inline IsC<T> print_1(const T& v);
 template<class T> inline NotC<T> print_1(const T& x) { cout << x; }
 template<size_t N> void print_1(const array<char, N>& x) { cout << &x[0]; };
@@ -142,13 +74,16 @@ template<size_t L, size_t I, class T> void print_tuple(const T& t) {
     if(I + 1 < L) print_tuple<L, (I + 1) % L>(t);
 }
 template<class ...T> inline void print_1(const tuple<T...>& x) {
-    cout << "("; print_tuple<sizeof...(T), 0, tuple<T...>>(x); cout << ")"; }
+    cout << "("; print_tuple<sizeof...(T), 0, tuple<T...>>(x); cout << ")";
+}
 inline void print_n() {}
 template<class T, class ...U> inline void print_n(const T& head, const U&... tail);
 template<class T, class U> inline void print_1(const pair<T, U>& p) {
-    cout << "("; print_1(p.first); cout << ", "; print_1(p.second); cout << ")"; }
+    cout << "("; print_1(p.first); cout << ", "; print_1(p.second); cout << ")";
+}
 template<class T, class ...U> inline void print_n(const T& head, const U&... tail) {
-    print_1(head); if(sizeof...(tail)) cout << " "; print_n(tail...); }
+    print_1(head); if(sizeof...(tail)) cout << " "; print_n(tail...);
+}
 template<class T> inline IsC<T> print_1(const T& v) {
     cout << "[";
     for(auto it = v.begin(); it != v.end(); ++it) { if(it != v.begin()) cout << ", "; print_1(*it); }
@@ -160,6 +95,20 @@ template<class T, class ...U> inline void read(T& head, U&... tail) { cin >> hea
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 static int fastio = [](){ ios_base::sync_with_stdio(false); cin.tie(0); cout.precision(17); return 0; }();
+
+template<class T> void print_dbg(const string& s, T&& x) {
+    if(is_const_char_arr_ref<T>::value) { print_n(x); } else { print_n(s, "=", x); }
+}
+#define SELECT(_1, _2, _3, _4, _5, _6, _7, _8, NAME,...) NAME
+#define dbg1(a) print_dbg(#a, a); cout << endl;
+#define dbg2(a, b) print_dbg(#a, a); cout << ", "; dbg1(b);
+#define dbg3(a, b, c) print_dbg(#a, a); cout << ", "; dbg2(b, c);
+#define dbg4(a, b, c, d) print_dbg(#a, a); cout << ", "; dbg3(b, c, d);
+#define dbg5(a, b, c, d, e) print_dbg(#a, a); cout << ", "; dbg4(b, c, d, e);
+#define dbg6(a, b, c, d, e, f) print_dbg(#a, a); cout << ", "; dbg5(b, c, d, e, f);
+#define dbg7(a, b, c, d, e, f, g) print_dbg(#a, a); cout << ", "; dbg6(b, c, d, e, f, g);
+#define dbg8(a, b, c, d, e, f, g, h) print_dbg(#a, a); cout << ", "; dbg7(b, c, d, e, f, g, h);
+#define debug(...) SELECT(__VA_ARGS__, dbg8, dbg7, dbg6, dbg5, dbg4, dbg3, dbg2, dbg1)(__VA_ARGS__)
 //}}}
 using PII = pair<int, int>;
 using LL = long long;
@@ -167,8 +116,9 @@ using LL = long long;
 const int MOD = 1e9 + 7;
 const int INF = 0x3f3f3f3f;
 const LL LLINF = 0x3f3f3f3f3f3f3f3f;
+const int MAX_R = 55, MAX_C = 55;
 
-struct SAT {
+struct SAT {//{{{
     vector<vector<int>> G;
     vector<int> vs;
     vector<char> color, visited;
@@ -245,119 +195,123 @@ struct SAT {
         }
         return res;
     }
-};
-
-const int MAX_R = 55, MAX_C = 55;
-auto sat = SAT(MAX_R * MAX_C);
+};//}}}
 
 int R, C;
 char room[MAX_R][MAX_C];
 
-bool go(int r, int c, array<int, 2> d, vector<array<int, 2>>& epts) {
-    r += d[0];
-    c += d[1];
-    while(r >= 0 and r < R and c >= 0 and c < C and room[r][c] != '#') {
-        if(room[r][c] == '.') {
-            epts.push_back({r, c});
-        } else if(room[r][c] == '|' or room[r][c] == '-') {
-            return false;
-        } else if(room[r][c] == '/') {
-            swap(d[0], d[1]);
-            d[0] = -d[0];
-            d[1] = -d[1];
-        } else if(room[r][c] == '\\') {
-            swap(d[0], d[1]);
-        }
+auto sat = SAT(MAX_R * MAX_C);
+
+struct Solution {
+    bool go(int r, int c, array<int, 2> d, vector<array<int, 2>>& epts) {
         r += d[0];
         c += d[1];
-    }
-    return true;
-}
-
-using VCondArr = array<array<vector<pair<int, bool>>, MAX_C>, MAX_R>;
-
-void explore(int r, int c, VCondArr& conds) {
-    vector<array<int, 2>> epts;
-    auto ok1 = go(r, c, {0, 1}, epts) and go(r, c, {0, -1}, epts);
-    if(not ok1) {
-        sat.add((r * C + c), false);
-    } else {
-        for(auto [r1, c1] : epts) {
-            conds[r1][c1].emplace_back(r * C + c, true);
+        while(r >= 0 and r < R and c >= 0 and c < C and room[r][c] != '#') {
+            if(room[r][c] == '.') {
+                epts.push_back({r, c});
+            } else if(room[r][c] == '|' or room[r][c] == '-') {
+                return false;
+            } else if(room[r][c] == '/') {
+                swap(d[0], d[1]);
+                d[0] = -d[0];
+                d[1] = -d[1];
+            } else if(room[r][c] == '\\') {
+                swap(d[0], d[1]);
+            }
+            r += d[0];
+            c += d[1];
         }
+        return true;
     }
 
-    epts.clear();
-    auto ok2 = go(r, c, {1, 0}, epts) and go(r, c, {-1, 0}, epts);
-    if(not ok2) {
-        sat.add((r * C + c), true);
-    } else {
-        for(auto [r1, c1] : epts) {
-            conds[r1][c1].emplace_back(r * C + c, false);
+    using VCondArr = array<array<vector<pair<int, bool>>, MAX_C>, MAX_R>;
+
+    void explore(int r, int c, VCondArr& conds) {
+        vector<array<int, 2>> epts;
+        auto ok1 = go(r, c, {0, 1}, epts) and go(r, c, {0, -1}, epts);
+        if(not ok1) {
+            sat.add((r * C + c), false);
+        } else {
+            for(auto& p : epts) {
+                int r1 = p[0], c1 = p[1];
+                conds[r1][c1].emplace_back(r * C + c, true);
+            }
         }
-    }
-}
 
-void solve() {
-    sat.clear();
-    read(R, C);
-    VCondArr conds;
-
-    for(int i : range(R)) {
-        read(room[i]);
-    }
-
-    for(int r : range(R)) {
-        for(int c : range(C)) {
-            if(room[r][c] == '|' or room[r][c] == '-') {
-                explore(r, c, conds);
+        epts.clear();
+        auto ok2 = go(r, c, {1, 0}, epts) and go(r, c, {-1, 0}, epts);
+        if(not ok2) {
+            sat.add((r * C + c), true);
+        } else {
+            for(auto& p : epts) {
+                int r1 = p[0], c1 = p[1];
+                conds[r1][c1].emplace_back(r * C + c, false);
             }
         }
     }
+    Solution(int) {
+        sat.clear();
+        read(R, C);
+        VCondArr conds;
 
-    for(int r : range(R)) {
-        for(int c : range(C)) {
-            if(room[r][c] != '.') {
-                continue;
-            }
-            if(conds[r][c].size() == 0 or conds[r][c].size() >= 3) {
-                print("IMPOSSIBLE");
-                return;
-            }
-            if(conds[r][c].size() == 1) {
-                auto [i0, b0] = conds[r][c][0];
-                sat.add(i0, b0);
-            } else {
-                auto [i0, b0] = conds[r][c][0];
-                auto [i1, b1] = conds[r][c][1];
-                sat.add(i0, b0, i1, b1);
+        for(int i : range(R)) {
+            read(room[i]);
+        }
+
+        for(int r : range(R)) {
+            for(int c : range(C)) {
+                if(room[r][c] == '|' or room[r][c] == '-') {
+                    explore(r, c, conds);
+                }
             }
         }
-    }
-    auto res = sat.solve();
-    if(res.empty()) {
-        print("IMPOSSIBLE");
-        return;
-    }
-    for(int r : range(R)) {
-        for(int c : range(C)) {
-            if(room[r][c] == '|' or room[r][c] == '-') {
-                room[r][c] = res[r * C + c] ? '-' : '|';
+
+        for(int r : range(R)) {
+            for(int c : range(C)) {
+                if(room[r][c] != '.') {
+                    continue;
+                }
+                if(conds[r][c].size() == 0 or conds[r][c].size() >= 3) {
+                    print("IMPOSSIBLE");
+                    return;
+                }
+                if(conds[r][c].size() == 1) {
+                    int i0, b0;
+                    tie(i0, b0) = conds[r][c][0];
+                    sat.add(i0, b0);
+                } else {
+                    int i0, b0, i1, b1;
+                    tie(i0, b0) = conds[r][c][0];
+                    tie(i1, b1) = conds[r][c][1];
+                    sat.add(i0, b0, i1, b1);
+                }
             }
         }
+        auto res = sat.solve();
+        if(res.empty()) {
+            print("IMPOSSIBLE");
+            return;
+        }
+        for(int r : range(R)) {
+            for(int c : range(C)) {
+                if(room[r][c] == '|' or room[r][c] == '-') {
+                    room[r][c] = res[r * C + c] ? '-' : '|';
+                }
+            }
+        }
+        print("POSSIBLE");
+        for(int i : range(R)) {
+            print(room[i]);
+        }
     }
-    print("POSSIBLE");
-    for(int i : range(R)) {
-        print(room[i]);
-    }
-}
+};
 
 int main() {
     int T;
     cin >> T;
     for(int i = 1; i <= T; ++i) {
         cout << "Case #" << i << ": ";
-        solve();
+        ignore = Solution(i);
     }
     return 0;
 }
